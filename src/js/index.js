5e9b10e2d338a17fa9bf1a328ea5ef4c965c7930
Jacob Cassidy <jacob@jacobcassidy.com>
2026-07-27 14:28:55 +0700

    Add `displayHeader()` to the init file

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 // Internal modules
+import { displayHeader } from "./modules/utils/displays.js";
 import { processArgs } from "./modules/utils/cli.js";
 
+displayHeader();
 processArgs();
