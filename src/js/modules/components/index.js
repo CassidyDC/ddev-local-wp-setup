3b65e455cfab61e2f695c440326a26836dd90ff7
Jacob Cassidy <jacob@jacobcassidy.com>
2026-07-28 06:14:33 +0700

    Add cliDebugInfo to components index

--- src/js/modules/components/index.js
+++ src/js/modules/components/index.js
@@ -1,3 +1,4 @@
+export { cliDebugInfo } from "./cli-debug-info.js";
 export { cliHeader } from "./cli-header.js";
 export { cliHelpMenu } from "./cli-help-menu.js";
 export { cliVersion } from "./cli-version.js";
