4f39295ca144d71022f54fb4ae68ca9fc95aa3f2
Jacob Cassidy <jacob@jacobcassidy.com>
2026-08-25 14:02:54 +0700

    Add prompts.js and validate.js to index file

--- src/js/modules/utils/helpers/index.js
+++ src/js/modules/utils/helpers/index.js
@@ -6,5 +6,7 @@
 export * from "./constants.js";
 export * from "./normalize.js";
 export * from "./process.js";
+export * from "./prompts.js";
 export * from "./shell.js";
 export * from "./styles.js";
+export * from "./validate.js";
