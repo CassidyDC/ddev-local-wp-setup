02337d089d203e15c29731e7afa45c6ba58c2e28
Jacob Cassidy <jacob@jacobcassidy.com>
2026-09-05 12:24:06 +0700

    Rename runInstall to runInstaller

--- src/js/modules/configs/commands.js
+++ src/js/modules/configs/commands.js
@@ -4,12 +4,12 @@
  */
 
 // Import commands
-import { runInstall } from "../commands/index.js";
+import { runInstaller } from "../commands/index.js";
 
 export const commandsConfig = {
   install: {
     alias: "i",
     description: "Runs the DDEV Local WP Setup installation wizard",
-    exec: runInstall,
+    exec: runInstaller,
   },
 };
