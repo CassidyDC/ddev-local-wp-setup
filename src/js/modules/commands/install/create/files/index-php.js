ea1358687b1df74fb420e88c39a3003347ae99fe
Jacob Cassidy <jacob@jacobcassidy.com>
2026-09-07 11:43:57 +0700

    Add index.php creation function

--- src/js/modules/commands/install/create/files/index-php.js
+++ src/js/modules/commands/install/create/files/index-php.js
@@ -3,7 +3,20 @@
  * @module commands/install/create/files/index-php
  */
 
-// - Add <root>/index.php file
-export async function createIndexPhpFile(dirPath) {
-  console.log(dirPath);
+// Import node modules
+import { writeFile } from "node:fs/promises";
+import path from "node:path";
+
+// Import helpers
+import { c, log } from "../../../../utils/helpers/index.js";
+
+/**
+ * Creates the <root>/index.php file.
+ *
+ * @param {string} rootDirPath The DDEV server root path.
+ * @param {string} wpCoreDir The dirname for the WordPress Core directory.
+ */
+export async function createIndexPhpFile(rootDirPath, wpCoreDir) {
+  log(c.detail("Creating `index.php` file..."));
+  await writeFile(path.join(rootDirPath, "index.php"), `<?php require_once __DIR__ '/${wpCoreDir}/index.php`);
 }
