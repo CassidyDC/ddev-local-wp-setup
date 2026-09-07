bfb421a03e070cf24f6437eb40e57668141f113b
Jacob Cassidy <jacob@jacobcassidy.com>
2026-09-07 11:44:17 +0700

    Add wp-cli.yml file creation functions

--- src/js/modules/commands/install/create/files/wp-cli.js
+++ src/js/modules/commands/install/create/files/wp-cli.js
@@ -3,7 +3,20 @@
  * @module commands/install/create/files/wp-cli
  */
 
-// - Add <root>/wp-cli.yml file
-export async function createWPCliFile(dirPath) {
-  console.log(dirPath);
+// Import node modules
+import { writeFile } from "node:fs/promises";
+import path from "node:path";
+
+// Import helpers
+import { c, log } from "../../../../utils/helpers/index.js";
+
+/**
+ * Creates the <root>/wp-cli.yml file to tell DDEV where to find the WP Core directory.
+ *
+ * @param {string} rootDirPath The DDEV server root path.
+ * @param {string} wpCoreDir The dirname for the WordPress Core directory.
+ */
+export async function createWPCliFile(rootDirPath, wpCoreDir) {
+  log(c.detail("Creating `wp-cli.yml` file..."));
+  await writeFile(path.join(rootDirPath, "wp-cli.yml"), `path: ${wpCoreDir}`);
 }
