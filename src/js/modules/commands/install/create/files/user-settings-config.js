6876724f1ddb3d0c1174180359430c3b44fae7eb
Jacob Cassidy <jacob@jacobcassidy.com>
2026-09-19 12:17:09 +0700

    Update config to remove password from being saved in created file

--- src/js/modules/commands/install/create/files/user-settings-config.js
+++ src/js/modules/commands/install/create/files/user-settings-config.js
@@ -11,13 +11,19 @@ import { writeFile } from "node:fs/promises";
 import { c, log, rootDirPath } from "../../../../utils/helpers/index.js";
 
 /**
- * Creates <root>/ddev-local-wp-setup-config.json file.
+ * Creates <root>/ddev-local-wp-setup-config.json without the WP admin password.
  */
 export async function createConfigFile(userSettingsConfig) {
   log(c.detail("Creating `ddev-local-wp-setup-config.json` file..."));
 
   const ddevLocalWPSetupConfigFile = path.join(`${rootDirPath}`, "ddev-local-wp-setup-config.json");
-  const ddevLocalWPSetupConfigFileContent = userSettingsConfig;
+  const configToSave = {
+    ...userSettingsConfig,
+    wordpress: { ...userSettingsConfig.wordpress },
+  };
+  delete configToSave.wordpress.adminPassword;
 
-  await writeFile(ddevLocalWPSetupConfigFile, ddevLocalWPSetupConfigFileContent);
+  const ddevLocalWPSetupConfigFileContent = JSON.stringify(configToSave, null, 2) + "\n";
+
+  await writeFile(ddevLocalWPSetupConfigFile, ddevLocalWPSetupConfigFileContent, "utf8");
 }
